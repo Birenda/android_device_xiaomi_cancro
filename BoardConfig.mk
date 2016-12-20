b0a03d3b9d1a75f307f502c091d8721530f1de7f
DrRamm <kotsareu@gmail.com>
2016-12-20 22:09:42 +0300

    temp disable dexpreopt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,15 +171,9 @@ BOARD_USES_QC_TIME_SERVICES := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-DONT_DEXPREOPT_PREBUILTS := true
+# Dexpreopt stuff
+WITH_DEXPREOPT := false
+#DONT_DEXPREOPT_PREBUILTS := true
 
 # SELinux policies
 # qcom sepolicy
