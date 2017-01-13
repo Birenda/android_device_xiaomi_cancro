a408374359ddc8f0295f4b507cb954c3efae3147
DrRamm <kotsareu@gmail.com>
2017-01-13 10:54:39 +0300

    xenonhd.mk: add a maintainer
    Change-Id: Id8771e78b6ba6fda0a2a85cb1602cabbec71123d

--- xenonhd.mk
+++ xenonhd.mk
@@ -19,8 +19,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common XenonHD stuff.
 $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+#XenonHD
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=DrRamm
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
