2925eaa748d595705d2cc91b3f261c23d4fb620f
DrRamm <kotsareu@gmail.com>
2017-01-20 10:05:51 +0300

    linageos
    Change-Id: Icc5a618f5ee4a1dc03b4d9caa45cd762e293fd00

--- lineage.mk
+++ lineage.mk
@@ -19,15 +19,13 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common XenonHD stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
-#XenonHD
-PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=DrRamm
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := xenonhd_cancro
+PRODUCT_NAME := lineage_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
